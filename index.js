b8461a8bddfd67bbaf77708b279758ad4a361bdb
srbharath <103111244+srbharath@users.noreply.github.com>
2023-05-05 12:52:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,49 +10,49 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json());
 
-const mysql = require('mysql2')
-const connection = mysql.createConnection({
-  host: '94ec7d7c1635',
-  user: 'root',
-  password: 'password',
-  database: 'test'
-})
+// const mysql = require('mysql2')
+// const connection = mysql.createConnection({
+//   host: '94ec7d7c1635',
+//   user: 'root',
+//   password: 'password',
+//   database: 'test'
+// })
 
-connection.connect()
-
-
+// connection.connect()
 
 
 
 
-app.get('/get',(req,res)=>{
 
-  connection.query('SELECT * from customers', (err, rows, fields) => {
-    if (err) throw err
 
-    res.json({rows})
+// app.get('/get',(req,res)=>{
+
+//   connection.query('SELECT * from customers', (err, rows, fields) => {
+//     if (err) throw err
+
+//     res.json({rows})
   
-  })
+//   })
 
 	
 
-})
+// })
 
-app.post('/insert',(req,res)=>{
+// app.post('/insert',(req,res)=>{
 
-  var CustomerID = req.body.CustomerID;
-  var CustomerName = req.body.CustomerName;
-  var ContactName = req.body.ContactName;
-  var Country = req.body.Country;
+//   var CustomerID = req.body.CustomerID;
+//   var CustomerName = req.body.CustomerName;
+//   var ContactName = req.body.ContactName;
+//   var Country = req.body.Country;
 
-  var sql = `INSERT INTO customers (CustomerID, CustomerName, ContactName, Country) VALUES ("${CustomerID}", "${CustomerName}", "${ContactName}", "${Country}")`;
+//   var sql = `INSERT INTO customers (CustomerID, CustomerName, ContactName, Country) VALUES ("${CustomerID}", "${CustomerName}", "${ContactName}", "${Country}")`;
 
   
 
-  connection.query(sql, function(err, result) {
-    if (err) throw err;
-    res.json({'message':"inserted"})   
-  });
+//   connection.query(sql, function(err, result) {
+//     if (err) throw err;
+//     res.json({'message':"inserted"})   
+//   });
 
   // connection.query('SELECT * from customers', (err, rows, fields) => {
   //   if (err) throw err
